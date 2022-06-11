9c3fc2423027c15d69129b543a0b7a6a17b5472e
Jon <jonathancerruti@gmail.com>
2022-06-11 03:24:58 -0700

    alert for password length slector with if statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,36 @@
 // Assignment code here
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var specialcharacters = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^","_","`","{","|","}",];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var specialcharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+function generatePassword (){
+  console.log("button clicked");
+  var passwordlength = prompt("Select password length between 8 and 128");
+      if (passwordlength < 8||passwordlength > 128){
+      alert("Password must be between 8 and 128 characters")
+      var passwordlength = prompt("Select password length between 8 and 128");
+      }
+      
+      
+
+
+  return "button clicked"
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-  console.log(password)
+  
 }
 
 // Add event listener to generate button
