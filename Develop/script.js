02d5809d55f426475d3ba26a803a837850bfe501
Jon <jonathancerruti@gmail.com>
2022-06-11 00:36:08 -0700

    variables for password characters defined

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
+
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialcharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +15,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// console.log(writePassword)
